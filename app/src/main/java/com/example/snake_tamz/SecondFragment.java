820c10c99d6e6b7640c7d366168d9a51ffa55a9d
Marcel <marcelgrygar7@gmail.com>
2020-11-30 01:20:52 +0100

    SnakeHead

--- app/src/main/java/com/example/snake_tamz/SecondFragment.java
+++ app/src/main/java/com/example/snake_tamz/SecondFragment.java
@@ -1,6 +1,14 @@
 package com.example.snake_tamz;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +19,47 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
 
+    Canvas canvas;
+    Bitmap headAnimBitmap;
+    Rect rectToBeDrawn;
+    SnakeAnimView snakeAnimView;
+
+    int frameHeight = 64;
+    int frameWidth = 64;
+    int numFrames = 6;
+    int frameNumber;
+
+    int screenwWidth;
+    int screenHeight;
+
+    long lastFrameTime;
+    int fps;
+    int hi;
+
+    Intent i;
+
+
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenHeight = size.y;
+        screenwWidth =size.x;
+
+        headAnimBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.head);
+        snakeAnimView = new SnakeAnimView(this);
+        setContentView(snakeAnimView);
+
+        i = new Intent(this,)
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
